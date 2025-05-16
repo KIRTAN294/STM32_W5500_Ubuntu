069787174872db37b0cf863db64291d4d057abb4
KIRTAN294 <kirtanp2922004@gmail.com>
2025-05-16 17:12:02 +0530

    New Changes

---
+++ STM32_Server/Inc/Normal_Plot.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+df = pd.read_csv('Normal_Data_01.csv')
+
+# df.insert(0, 'sr.no', range(1, len(df) + 1))
+expected_interval = 1
+
+plt.figure(figsize=(13,7))
+df ['jitter'] = df['Final_Time'].fillna(1)-1
+# df = df.drop (columns=["Source","Destination","Protocol","Length","Info"])
+# df = df.drop (columns=["Sr.No"])
+df["Final_Time"] = (df["Time"].diff())*1000
+df.to_csv("Normal_Data_01.csv",index=False)
+print(df)
+
+# sns.boxplot(x="sr.no", y="Final_Time", data=df)
+maxvlaue = df['Final_Time'].max()
+minvalue = df['Final_Time'].min()
+print(maxvlaue)
+print(minvalue)
+
+# plt.subplot(1,2,1)
+# plt.hist(df['Final_Time'],bins=np.arange(0,3.2,0.2),edgecolor='black')
+# plt.xticks(ticks=np.arange(0,3.2,0.2)) 
+# plt.xlabel('Final_Time')
+# plt.ylabel('Frequency')
+# plt.grid(True)
+# plt.legend()
+
+print(df.head())
+
+# # plt.subplot(1,2,2)
+plt.plot (df['sr.no'].values,df['Final_Time'].values,color='blue',marker='o',linestyle='-')
+plt.axhline(y=expected_interval,color='red',linestyle='--')
+plt.xlabel('Sr.no')
+plt.ylabel('Time')
+plt.legend()
+plt.title('Normal Packet timing')
+plt.grid(True)
+plt.show()
